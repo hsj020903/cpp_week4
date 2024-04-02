0f4f4e73463bb46a1321da9b005b21e76632db1f
hsj020903 <hsj020903@naver.com>
2024-04-03 04:55:47 +0900

    데이터베이스 구현

---
+++ database.cpp
@@ -0,0 +1,136 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+#define DATABASE_H
+
+enum Type
+{
+    INT,
+    DOUBLE,
+    STRING,
+    ARRAY
+};
+
+struct Array
+{
+    int size;
+    Type type;
+    void *items;
+};
+
+struct Entry
+{
+    Type type;
+    std::string key;
+    void *value;
+};
+
+struct Database
+{
+    int datasize;
+    Entry *entry;
+};
+
+// 엔트리를 생성한다.
+Entry *create(Type type, std::string key, void *value)
+{
+    Entry *entry = new Entry;
+    entry->type = type;
+    entry->key = key;
+    entry->value = value;
+    return entry;
+}
+
+// 데이터베이스를 초기화한다.
+void init(Database &database)
+{
+    database.datasize = 0;
+    delete[] database.entry;
+    database.entry = new Entry[1];
+}
+
+// 데이터베이스에 엔트리를 추가한다.
+void add(Database &database, Entry *entry)
+{
+    int count = -1;
+    database.datasize++;
+    Entry *newData = new Entry[database.datasize];
+    for (int i = 0; i < database.datasize - 1; i++)
+    {
+        newData[i] = database.entry[i];
+        if (database.entry[i].key == entry->key)
+            count = i;
+    }
+
+    if (count == -1)
+    {
+        *(newData + database.datasize - 1) = *entry;
+    }
+    else
+    {
+        *(newData + count) = *entry;
+        database.datasize--;
+    }
+
+    delete[] database.entry;
+    database.entry = newData;
+    delete entry;
+}
+
+// 데이터베이스에서 키에 해당하는 엔트리를 찾는다.
+Entry *get(Database &database, std::string &key)
+{
+    for (int i = 0; i < database.datasize; i++)
+    {
+        if (database.entry[i].key == key)
+        {
+            return &database.entry[i];
+        }
+    }
+    string n = "none";
+    Entry *none = create(STRING, "none", &n);
+    return none;
+}
+
+// 데이터베이스에서 키에 해당하는 엔트리를 제거한다.
+void remove(Database &database, std::string &key)
+{
+
+    int check = 0, count = 0;
+     Entry *data;
+    for (int i = 0; i < database.datasize; i++)
+    {
+        if (database.entry[i].key == key)
+        {
+            check++;
+            database.datasize -= 1;
+            if (database.datasize != 0)
+            {
+                data = new Entry[database.datasize];
+                for (int j = 0; j < database.datasize+1; j++)
+                {
+                    if (i != j)
+                    {
+                        data[count] = database.entry[j];
+                        count++;
+                    }
+                }
+            }
+            else{
+                data = new Entry[1];
+            }
+            delete[] database.entry;
+            database.entry = data;
+            break;
+        }
+    }
+    if (check == 0)
+        cout << "not found";
+}
+
+// 데이터베이스를 해제한다.
+void destroy(Database &database)
+{
+    delete[] &database;
+}
---
+++ main.cpp
@@ -0,0 +1,287 @@
+#include "database.h"
+#include <string>
+#include <iostream>
+using namespace std;
+#define typepic "type (int, double, string, array) : "
+// 끝없이 값을 받을 수 있는 배열선언 함수
+int arrays(Array &arr)
+{
+    string s, sval;
+    int size, ival;
+    double dval;
+    cout << typepic;
+    cin >> s;
+    if (s != "int" && s != "double" && s != "string" && s != "array")
+    {
+        cout << "invalid type" << endl;
+        return 0;
+        ;
+    }
+    cout << "size: ";
+    if (!(cin >> size))
+    {
+        cin.clear();
+        while (std::cin.get() == '\n')
+            continue;
+        cout << "invalid size" << endl;
+        return 0;
+    }
+    arr.size = size;
+    if (s == "int")
+    {
+        arr.type = INT;
+        int *item = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            cout << "item[" << i << "]: ";
+            if (!(cin >> ival))
+            {
+                cin.clear();
+                while (std::cin.get() == '\n')
+                    continue;
+                cout << "invalid int" << endl;
+                return 0;
+            }
+            item[i] = ival;
+        }
+        arr.items = item;
+    }
+    if (s == "double")
+    {
+        arr.type = DOUBLE;
+        double *item = new double[size];
+        for (int i = 0; i < size; i++)
+        {
+            cout << "item[" << i << "]: ";
+            if (!(cin >> dval))
+            {
+                cin.clear();
+                while (std::cin.get() == '\n')
+                    continue;
+                cout << "invalid int" << endl;
+                return 0;
+            }
+            item[i] = dval;
+        }
+        arr.items = item;
+    }
+    if (s == "string")
+    {
+        arr.type = STRING;
+        string *item = new string[size];
+        for (int i = 0; i < size; i++)
+        {
+            cout << "item[" << i << "]: ";
+            cin.ignore();
+            getline(cin, sval);
+            item[i] = sval;
+        }
+        arr.items = item;
+    }
+    if (s == "array")
+    {
+        arr.type = ARRAY;
+        Array *item = new Array[size];
+        for (int i = 0; i < size; i++)
+        {
+            cout << "item[" << i << "]: ";
+            int k = arrays(item[i]);
+            if (k == 0)
+                return 0;
+        }
+        arr.items = item;
+    }
+    return 1;
+}
+
+// 배열 출력기
+void arrPrint(Array &arr, int n)
+{
+    cout << "[";
+    for (int i = 0; i < arr.size; i++)
+    {
+        switch (arr.type)
+        {
+        case INT:
+            cout << static_cast<int *>(arr.items)[i];
+            if (i + 1 != arr.size)
+                cout << ", ";
+            break;
+        case DOUBLE:
+            cout << static_cast<double *>(arr.items)[i];
+            if (i + 1 != arr.size)
+                cout << ", ";
+            break;
+        case STRING:
+            cout << "\"";
+            cout << static_cast<string *>(arr.items)[i];
+            cout << "\"";
+            if (i + 1 != arr.size)
+                cout << ", ";
+            break;
+        case ARRAY:
+            arrPrint(static_cast<Array *>(arr.items)[i], arr.size - i - 1);
+            break;
+        default:
+            break;
+        }
+    }
+    cout << "]";
+    if (n != 0)
+        cout << ", ";
+}
+
+int main()
+{
+    string s, key;
+    string sval;
+    int ival;
+    double dval;
+    Type type;
+    bool end = false;
+    Database database;
+    init(database);
+    while (!end)
+    {
+        cout << "command (list, add, get, del, exit): ";
+        cin >> s;
+        if (s == "exit")
+        {
+            break;
+        }
+        else if (s == "list")
+        {
+            for (int i = 0; i < database.datasize; i++)
+            {
+                switch (database.entry[i].type)
+                {
+                case INT:
+                    cout << database.entry[i].key << ": " << *static_cast<int *>(database.entry[i].value) << endl;
+                    break;
+                case DOUBLE:
+                    cout << database.entry[i].key << ": " << *static_cast<double *>(database.entry[i].value) << endl;
+                    break;
+                case STRING:
+                    cout << database.entry[i].key << ": "
+                         << "\"" << *static_cast<string *>(database.entry[i].value) << "\"" << endl;
+                    break;
+                case ARRAY:
+                    cout << database.entry[i].key << ": ";
+                    arrPrint(*static_cast<Array *>(database.entry[i].value), 0);
+                    cout << endl;
+                    break;
+                default:
+                    break;
+                }
+                ((database.entry + i)->type);
+            }
+        }
+        else if (s == "add")
+        { // 엔트리 추가 입력
+            cout << "key: ";
+            cin >> key;
+            cout << typepic;
+            cin >> s;
+            if (s != "int" && s != "double" && s != "string" && s != "array")
+            {
+                cout << "invalid type" << endl;
+                continue;
+            }
+
+            cout << "value: ";
+            if (s == "int")
+            {
+                type = INT;
+                if (!(cin >> ival))
+                {
+                    cin.clear();
+                    while (std::cin.get() == '\n')
+                        continue;
+                    cout << "invalid int" << endl;
+                    continue;
+                }
+
+                Entry *entry = create(type, key, &ival);
+                add(database, entry);
+            }
+            else if (s == "double")
+            {
+                type = DOUBLE;
+                if (!(cin >> dval))
+                {
+                    cin.clear();
+                    while (std::cin.get() == '\n')
+                        continue;
+                    cout << "invalid double" << endl;
+                    continue;
+                }
+                Entry *entry = create(type, key, &dval);
+                add(database, entry);
+            }
+            else if (s == "string")
+            {
+                type = STRING;
+                cin.ignore();
+                getline(cin, sval);
+                Entry *entry = create(type, key, &sval);
+                add(database, entry);
+            }
+            else if (s == "array")
+            {
+                Array arr;
+                type = ARRAY;
+                int k = arrays(arr);
+                if (k == 0)
+                {
+                    continue;
+                }
+                Entry *entry = create(type, key, &arr);
+                add(database, entry);
+            }
+        }
+
+        else if (s == "get") // 데이터베이스에서 엔트리를 찾는다
+        {
+            cout << "key: ";
+            cin >> key;
+            Entry *entry = get(database, key);
+            if (entry->key != "none")
+            {
+                switch (entry->type)
+                {
+                case INT:
+                    cout << (entry->key) << ": " << *static_cast<int *>(entry->value) << endl;
+                    break;
+                case DOUBLE:
+                    cout << (entry->key) << ": " << *static_cast<double *>(entry->value) << endl;
+                    break;
+                case STRING:
+                    cout << (entry->key) << ": " << *static_cast<string *>(entry->value) << endl;
+                    break;
+                case ARRAY:
+                    cout << entry->key << ": ";
+                    arrPrint(*static_cast<Array *>(entry->value), 0);
+                    cout << endl;
+                    break;
+                default:
+                    break;
+                }
+            }
+            else
+                cout << "not found" << endl;
+        }
+
+        else if (s == "del") // 데이터베이스에서 엔트리를 삭제한다
+        {
+            cout << "key: ";
+            cin >> key;
+            remove(database, key);
+        }
+        else
+        {
+            cout << "invalid command" << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
